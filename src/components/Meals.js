a40775994fccc246b6e5ae38c2fac0906b004a40
M1tras <108801657+M1tras@users.noreply.github.com>
2026-03-20 11:21:19 +0000

    ylesanne3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react"
+
 const Meals = () => {
+
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        fetch('https://curly-guacamole-x5x69vgg9gxhvvgw-3001.app.github.dev/meals').then(res => res.json()).then(data => setMeals(data))
+    }, [])
+
+
+    console.log(meals)
     return (
+        <>
+        <h2>create list of meals, using fetch data from backend</h2>
         <ul id="meals">
             { 
                 // list of meals
             }
         </ul>
+        </>
     )
 }
 
